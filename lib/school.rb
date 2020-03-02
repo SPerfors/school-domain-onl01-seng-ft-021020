67c2deab1db24920f47f3e5098b28abffcae15b0
Steven Perfors <stevecanbuild@gmail.com>
2020-03-02 20:28:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,14 @@
 class School 
-  attr_accessor :name, :roster 
+  attr_accessor :name, :roster, :grade
   
   #roster = {}
   
   def initialize(name)
     @name = name 
     @roster = {}
+  end
+  
+  def add_student(name, grade)
   end
   
 end
